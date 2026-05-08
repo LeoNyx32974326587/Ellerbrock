e1613127110a1403766366721a1f5f2b3b024659
LeoNyx32974326587 <leon.luca.lauterbach@gmail.com>
2026-05-08 17:16:55 +0200

    Bilder aktualisiert

--- image-config.js
+++ image-config.js
@@ -1,7 +1,7 @@
 window.ELLERBROCK_IMAGES = {
   "team-david-hintenlang.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238378/ellerbrock/team-david-hintenlang.webp",
-  "team-joerg-raphael.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238396/ellerbrock/team-joerg-raphael.webp",
-  "team-fabian-habel.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238403/ellerbrock/team-fabian-habel.webp",
+  "team-joerg-raphael.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778253228/ellerbrock/team-joerg-raphael-1778253227705.webp",
+  "team-fabian-habel.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778253242/ellerbrock/team-fabian-habel-1778253242551.webp",
   "dolmetscheranlage-einsatz.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238769/ellerbrock/dolmetscheranlage-einsatz.jpg",
   "galerie-1.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778248772/ellerbrock/galerie-1-1778248771364.webp"
 };
