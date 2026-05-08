75021829bd340f49efa3b7a59d8244d0f42323f1
LeoNyx32974326587 <leon.luca.lauterbach@gmail.com>
2026-05-08 17:21:54 +0200

    Bilder aktualisiert

--- image-config.js
+++ image-config.js
@@ -1,7 +1,8 @@
 window.ELLERBROCK_IMAGES = {
   "team-david-hintenlang.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238378/ellerbrock/team-david-hintenlang.webp",
-  "team-joerg-raphael.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778253228/ellerbrock/team-joerg-raphael-1778253227705.webp",
-  "team-fabian-habel.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778253242/ellerbrock/team-fabian-habel-1778253242551.webp",
+  "team-joerg-raphael.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778253646/ellerbrock/team-joerg-raphael-1778253645039.webp",
+  "team-fabian-habel.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778253650/ellerbrock/team-fabian-habel-1778253649968.webp",
   "dolmetscheranlage-einsatz.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238769/ellerbrock/dolmetscheranlage-einsatz.jpg",
-  "galerie-1.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778248772/ellerbrock/galerie-1-1778248771364.webp"
+  "galerie-1.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778248772/ellerbrock/galerie-1-1778248771364.webp",
+  "team-julius-lampert.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778253700/ellerbrock/team-julius-lampert-1778253700216.jpg"
 };
