53181cfbc87b3bbeb437aeb8ff543eedc14af6ad
LeoNyx32974326587 <leon.luca.lauterbach@gmail.com>
2026-05-08 16:01:45 +0200

    Test image config

--- image-config.js
+++ image-config.js
@@ -1,7 +1,3 @@
 window.ELLERBROCK_IMAGES = {
-  "team-david-hintenlang.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238378/ellerbrock/team-david-hintenlang.webp",
-  "team-joerg-raphael.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238396/ellerbrock/team-joerg-raphael.webp",
-  "team-fabian-habel.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238403/ellerbrock/team-fabian-habel.webp",
-  "dolmetscheranlage-einsatz.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238769/ellerbrock/dolmetscheranlage-einsatz.jpg",
-  "galerie-1.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778248772/ellerbrock/galerie-1-1778248771364.webp"
+  "test.jpg": "https://example.com/test.jpg"
 };
