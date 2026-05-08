1afc12423bbaeceef7c453d85a8020b5813e00d3
LeoNyx32974326587 <leon.luca.lauterbach@gmail.com>
2026-05-08 14:19:40 +0200

    Add files via upload

--- image-config.js
+++ image-config.js
@@ -1,7 +1,1 @@
-window.ELLERBROCK_IMAGES = {
-  "team-david-hintenlang.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238378/ellerbrock/team-david-hintenlang.webp",
-  "team-joerg-raphael.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238396/ellerbrock/team-joerg-raphael.webp",
-  "team-fabian-habel.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238403/ellerbrock/team-fabian-habel.webp",
-  "dolmetscheranlage-einsatz.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778238769/ellerbrock/dolmetscheranlage-einsatz.jpg",
-  "galerie-1.jpg": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778242453/ellerbrock/galerie-1-1778242451998.webp"
-};
+window.ELLERBROCK_IMAGES = {};
