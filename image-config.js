b1fc9f03c380060b77e79230b3f6956843f86b04
LeoNyx32974326587 <leon.luca.lauterbach@gmail.com>
2026-05-11 11:39:53 +0200

    Bilder aktualisiert

--- image-config.js
+++ image-config.js
@@ -11,7 +11,7 @@ window.ELLERBROCK_IMAGES = {
   },
   "galerie-1.jpg": {
     "url": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778485590/ellerbrock/galerie-1-1778485590684.jpg",
-    "fit": "cover",
+    "fit": "contain",
     "pos": "center"
   },
   "team-fabian-ellerbrock.jpg": {
@@ -72,7 +72,10 @@ window.ELLERBROCK_IMAGES = {
       "section>div:nth-of-type(2)>div:nth-of-type(3)>div:nth-of-type(3)>strong": "12.000+",
       "section>div:nth-of-type(2)>div:nth-of-type(3)>div:nth-of-type(3)>span": "Empfänger weltweit",
       "section:nth-of-type(3)>div>div>span": "Referenzen",
-      "div:nth-of-type(2)>div:nth-of-type(2)>div>div:nth-of-type(3)>div>div:nth-of-type(2)>div:nth-of-type(2)>p": "Verantwortet den operativen Bereich und die technische Leitung — von der Projektplanung bis zur Vor-Ort-Betreuung"
+      "div:nth-of-type(2)>div:nth-of-type(2)>div>div:nth-of-type(3)>div>div:nth-of-type(2)>div:nth-of-type(2)>p": "Verantwortet den operativen Bereich und die technische Leitung — von der Projektplanung bis zur Vor-Ort-Betreuung",
+      "div:nth-of-type(2)>div:nth-of-type(2)>div>div>div>form>div:nth-of-type(3)>label": "Betreff *",
+      "nav>div>a": "\n      coverUpload⋯\n    ",
+      "section:nth-of-type(6)>div>div:nth-of-type(2)>a:nth-of-type(2)": "containUpload⋯"
     }
   }
 };
