d29c8d9bfe9953af270e5853672b8faa8302562a
LeoNyx32974326587 <leon.luca.lauterbach@gmail.com>
2026-05-11 09:54:47 +0200

    Bilder aktualisiert

--- image-config.js
+++ image-config.js
@@ -18,5 +18,10 @@ window.ELLERBROCK_IMAGES = {
     "url": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778485590/ellerbrock/galerie-1-1778485590684.jpg",
     "fit": "cover",
     "pos": "center"
+  },
+  "team-fabian-ellerbrock.jpg": {
+    "url": "https://res.cloudinary.com/daiiz3u5t/image/upload/v1778486070/ellerbrock/team-fabian-ellerbrock-1778486070354.jpg",
+    "fit": "cover",
+    "pos": "center"
   }
 };
